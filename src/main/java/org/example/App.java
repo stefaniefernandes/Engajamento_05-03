b7b4f969a171e9afd7f4ecb2897579c2044fffa7
eduardo82926 <eduardo82926@gmail.com>
2020-03-05 20:50:05 -0300

    insert exibir lista

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,22 +5,46 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-/**
- * Hello world!
- */
+
 public class App {
+
+    private static Scanner sc = new Scanner(System.in);
+    private static List<Aluno> listaAlunos = new ArrayList<Aluno>();
+
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        List<Aluno> listaAlunos = new ArrayList<Aluno>();
 
         int i = 0;
-        int option = 0;
-        while(1 != 0) {
-            System.out.println("Insira do nome: ");
-            String nome = sc.nextLine();
-            listaAlunos.add(new Aluno(nome));
-            System.out.println(i + " Nome cadastrado: " + listaAlunos.get(i).getNome());
-            i++;
+        String option;
+
+        do {
+
+            System.out.println("----- Menu -----");
+            System.out.println("[1] - Inserir Aluno");
+            System.out.println("[2] - Imprimir Lista\n");
+            System.out.println("[0] - Sair\n");
+            option = sc.nextLine();
+
+            switch (option) {
+                case "1":
+                    inserirAluno();
+                    break;
+                case "2":
+                    exibirLista();
+                    break;
+            }
+        } while (!option.equals("2"));
+    }
+
+    private static void inserirAluno() {
+        System.out.println("Insira o nome: ");
+        String nome = sc.nextLine();
+        listaAlunos.add(new Aluno(nome));
+    }
+
+    private static void exibirLista() {
+        System.out.println("----- Lista de nomes ----");
+        for (Aluno aluno : listaAlunos){
+            System.out.println(aluno.getNome());
         }
     }
 }
